f534dca8c52c0f22acc5133d45aa952336964969
José Martínez <marquevjosman@gmail.com>
2024-06-06 04:33:09 -0600

    up

--- c/source/main.c
+++ c/source/main.c
@@ -1,26 +1,62 @@
-#include <fcntl.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-struct Estructura;
+#define mznull NULL
 
-int main(void) {
-  char *device = "/dev/midi2";
-  unsigned char data[3] = {0x90, 60, 127};
+struct mzg {
+  double scale[12];
+  double *samp;
+  int fram;
+  int chan;
+};
 
-  // step 1: open the OSS device for writing
-  int fd = open(device, O_WRONLY, 0);
-  if (fd < 0) {
-    printf("Error: cannot open %s\n", device);
-    exit(1);
-  }
+struct mzg mzgvar;
 
-  // step 2: write the MIDI information to the OSS device
-  write(fd, data, sizeof(data));
+struct mzcmd {
+  double freq;
+  double time;
+  double dur;
+  double amp;
+};
 
-  // step 3: (optional) close the OSS device
-  close(fd);
+double mzequaltempered(double note, double basefreq) {
+  return basefreq * pow(2, note / 12);
+}
 
-  return 0;
+void mzinit() {
+  mzgvar.scale[0] = (double)1;
+  mzgvar.scale[1] = (double)256 / 243;
+  mzgvar.scale[2] = (double)9 / 8;
+  mzgvar.scale[3] = (double)32 / 27;
+  mzgvar.scale[4] = (double)81 / 64;
+  mzgvar.scale[5] = (double)4 / 3;
+  mzgvar.scale[6] = mzequaltempered(6, 1);
+  mzgvar.scale[7] = (double)3 / 2;
+  mzgvar.scale[8] = (double)128 / 81;
+  mzgvar.scale[9] = (double)27 / 16;
+  mzgvar.scale[10] = (double)16 / 9;
+  mzgvar.scale[11] = (double)243 / 128;
+  mzgvar.samp = mznull;
+  mzgvar.fram = 0;
+  mzgvar.chan = 2;
+}
+
+double mznote(int ration, int octave) {
+  return mzgvar.scale[ration] * pow(2, octave);
+}
+
+void mzend(const char *path) {}
+
+void mzadd(struct mzcmd cmd) {}
+
+int main() {
+  mzinit();
+  struct mzcmd cmd;
+  cmd.freq = mznote(0, 4);
+  cmd.time = 0;
+  cmd.dur = 1;
+  cmd.amp = 1;
+  mzadd(cmd);
+  mzend("out/song.wav");
 }
